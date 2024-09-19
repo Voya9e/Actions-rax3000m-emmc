53b9c8ab45e1823ac70656b40c6e026690f7259d
Cuptain <33347649+Voya9e@users.noreply.github.com>
2024-09-19 23:06:07 +0800

    Update diy.sh

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -12,7 +12,7 @@ sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generat
 # 添加软件源
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 # sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
-sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+# sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 
 #添加luci-app-adguardhome
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
