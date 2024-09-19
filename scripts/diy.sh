1134db7209cd6d5c6d605fe76963799feaae7ff2
Cuptain <33347649+Voya9e@users.noreply.github.com>
2024-09-19 23:05:56 +0800

    Update diy.sh

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -9,14 +9,16 @@ export Ttyd_account_free_login="0"
 #更改默认地址为192.168.6.1
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 
-# Add a feed source
-# sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
-
-# Add a feed source
+# 添加软件源
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 
 #添加luci-app-adguardhome
-git clone https://github.com/kiddin9/openwrt-adguardhome/tree/master/luci-app-adguardhome luci/luci-app-adguardhome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+
+#添加luci-app-turboacc
+curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
 
 # 增加AdGuardHome插件和核心
 export AdGuardHome_Core="1"
