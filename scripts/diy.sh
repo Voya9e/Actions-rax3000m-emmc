3a2596d2623bcdc2daaa33c9899b41db795b0ee3
Cuptain <33347649+Voya9e@users.noreply.github.com>
2024-09-19 22:28:37 +0800

    Update diy.sh

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -10,13 +10,13 @@ export Ttyd_account_free_login="0"
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 
 # Add a feed source
-sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
+# sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 #添加luci-app-adguardhome
-#git clone https://github.com/kiddin9/openwrt-adguardhome/tree/master/luci-app-adguardhome package/luci-app-adguardhome
+git clone https://github.com/kiddin9/openwrt-adguardhome/tree/master/luci-app-adguardhome luci/luci-app-adguardhome
 
 # 增加AdGuardHome插件和核心
 export AdGuardHome_Core="1"
