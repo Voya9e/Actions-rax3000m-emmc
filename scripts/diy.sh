5ec7c92297a8dd229b7f1cc38cb64619a30b5655
Cuptain <33347649+Voya9e@users.noreply.github.com>
2024-09-19 21:40:28 +0800

    Update diy.sh

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -9,11 +9,14 @@ export Ttyd_account_free_login="0"
 #更改默认地址为192.168.6.1
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 
-#添加adguardhome
-git clone https://github.com/kiddin9/openwrt-adguardhome/tree/master/adguardhome package/adguardhome
+# Add a feed source
+sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
+
+# Add a feed source
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 #添加luci-app-adguardhome
-git clone https://github.com/kiddin9/openwrt-adguardhome/tree/master/luci-app-adguardhome package/luci-app-adguardhome
+#git clone https://github.com/kiddin9/openwrt-adguardhome/tree/master/luci-app-adguardhome package/luci-app-adguardhome
 
 # 增加AdGuardHome插件和核心
 export AdGuardHome_Core="1"
